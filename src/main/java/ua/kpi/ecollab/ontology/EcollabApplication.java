44e76b5799e0515f1b287a8c8299665a732ac346
Oleksii <alexeyyermosin@gmail.com>
2023-05-10 00:49:27 +0300

    Initial commit

---
+++ src/main/java/ua/kpi/ecollab/ontology/EcollabApplication.java
@@ -0,0 +1,13 @@
+package ua.kpi.ecollab.ontology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcollabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcollabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/kpi/ecollab/ontology/EcollabApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.kpi.ecollab.ontology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcollabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
